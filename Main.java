20d698afae952a7b88664024d9574ec859372f01
Chase Harmon <93667899+chase6harmon@users.noreply.github.com>
2021-11-03 16:11:58 +0000

    Added print statement

--- Main.java
+++ Main.java
@@ -1,5 +1,8 @@
 class Main {
   public static void main(String[] args) {
     System.out.println("Hello world!");
+    System.out.println("I am awesome and this is my code");
+    int one = 2; 
+    System.out.println(one);
   }
 }
